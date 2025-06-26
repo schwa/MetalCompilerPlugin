9142c99c9d72313eaddd2e52377feff491d4ed19
Jonathan Wight <jwight@mac.com>
2025-06-26 10:44:16 -0700

    Require macOS 14 (sonoma) and bump swift-tools-version up to reflect that.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MetalCompilerPlugin",
+    platforms: [
+        .macOS(.v14),
+    ],
     products: [
         .plugin(name: "MetalCompilerPlugin", targets: ["MetalCompilerPlugin"]),
     ],
